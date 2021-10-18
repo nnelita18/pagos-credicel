d2e35e93c3b4bae93e1fa110adc1f469f1778f6a
nnelita18 <antonella.march27@gmail.com>
2021-10-18 11:06:35 -0700

    Adding theme

--- assets/js/page/modules-slider.js
+++ assets/js/page/modules-slider.js
@@ -1,7 +1,18 @@
 "use strict";
 
-$("#slider1,#slider2").owlCarousel({
-  items: 1,
+$("#slider1").owlCarousel({
+  // items: 1,
+  // nav: true,
+  items:1,
+  lazyLoad:true,
+  loop:true,
+  margin:10,
   nav: true,
-  navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
+  navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+  animateOut: 'slideOutDown',
+  animateIn: 'flipInX',
+  autoplay:true,
+  dots: false,
+  // autoplayTimeout:1000,
+  autoplayHoverPause:true
 });
